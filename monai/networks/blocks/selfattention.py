dd4e770f97a48a5e9f542621dcf58b42e8c240b0
vgrau98 <victor.grau93@gmail.com>
2023-12-29 10:40:08 +0100

    feat: rel pos embedding in attention block
    Signed-off-by: vgrau98 <victor.grau93@gmail.com>

--- monai/networks/blocks/selfattention.py
+++ monai/networks/blocks/selfattention.py
@@ -11,8 +11,11 @@
 
 from __future__ import annotations
 
+from typing import Tuple, Optional
+
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from monai.utils import optional_import
 
@@ -31,6 +34,8 @@ class SABlock(nn.Module):
         num_heads: int,
         dropout_rate: float = 0.0,
         qkv_bias: bool = False,
+        use_rel_pos: bool = False,
+        input_size: Optional[Tuple[int, int]] = None,
         save_attn: bool = False,
     ) -> None:
         """
@@ -63,10 +68,44 @@ class SABlock(nn.Module):
         self.save_attn = save_attn
         self.att_mat = torch.Tensor()
 
-    def forward(self, x):
+        self.use_rel_pos = use_rel_pos
+        if self.use_rel_pos:
+            assert input_size is not None, "Input size must be provided if using relative positional encoding."
+            # initialize relative positional embeddings
+            self.rel_pos_h = nn.Parameter(torch.zeros(2 * input_size[0] - 1, self.head_dim))
+            self.rel_pos_w = nn.Parameter(torch.zeros(2 * input_size[1] - 1, self.head_dim))
+
+    def forward(self, x: torch.Tensor):
+        print(f"in attention bloc, x shape: {x.shape}")
         output = self.input_rearrange(self.qkv(x))
         q, k, v = output[0], output[1], output[2]
-        att_mat = (torch.einsum("blxd,blyd->blxy", q, k) * self.scale).softmax(dim=-1)
+        # att_mat = (torch.einsum("blxd,blyd->blxy", q, k) * self.scale).softmax(dim=-1)
+        att_mat = torch.einsum("blxd,blyd->blxy", q, k) * self.scale
+
+        if self.use_rel_pos:
+            B, img_s_square, _ = x.shape
+            print(f"img_s_square: {img_s_square}")
+            img_s = int(img_s_square**0.5)
+            print(f"img_s: {img_s}")
+            # att_mat = att_mat.reshape(B, img_s, img_s, -1)
+            print(f"att_mat shape: {att_mat.shape}")
+            att_mat = add_decomposed_rel_pos(
+                att_mat.view(B * self.num_heads, img_s_square, img_s_square),
+                q.view(B * self.num_heads, img_s_square, -1),
+                self.rel_pos_h,
+                self.rel_pos_w,
+                (img_s, img_s),
+                (img_s, img_s),
+            )
+            att_mat = att_mat.reshape(
+                B,
+                self.num_heads,
+                img_s_square,
+                img_s_square,
+            )
+            print(f"att mat after pos_embedding: {att_mat.shape}")
+        print(f"att mat before softmax: {att_mat.shape}")
+        att_mat = att_mat.softmax(dim=-1)
         if self.save_attn:
             # no gradients and new tensor;
             # https://pytorch.org/docs/stable/generated/torch.Tensor.detach.html
@@ -78,3 +117,76 @@ class SABlock(nn.Module):
         x = self.out_proj(x)
         x = self.drop_output(x)
         return x
+
+
+def get_rel_pos(q_size: int, k_size: int, rel_pos: torch.Tensor) -> torch.Tensor:
+    """
+    Get relative positional embeddings according to the relative positions of
+        query and key sizes.
+    Args:
+        q_size (int): size of query q.
+        k_size (int): size of key k.
+        rel_pos (Tensor): relative position embeddings (L, C).
+
+    Returns:
+        Extracted positional embeddings according to relative positions.
+    """
+    max_rel_dist = int(2 * max(q_size, k_size) - 1)
+    # Interpolate rel pos if needed.
+    if rel_pos.shape[0] != max_rel_dist:
+        # Interpolate rel pos.
+        rel_pos_resized = F.interpolate(
+            rel_pos.reshape(1, rel_pos.shape[0], -1).permute(0, 2, 1),
+            size=max_rel_dist,
+            mode="linear",
+        )
+        rel_pos_resized = rel_pos_resized.reshape(-1, max_rel_dist).permute(1, 0)
+    else:
+        rel_pos_resized = rel_pos
+
+    # Scale the coords with short length if shapes for q and k are different.
+    q_coords = torch.arange(q_size)[:, None] * max(k_size / q_size, 1.0)
+    k_coords = torch.arange(k_size)[None, :] * max(q_size / k_size, 1.0)
+    relative_coords = (q_coords - k_coords) + (k_size - 1) * max(q_size / k_size, 1.0)
+
+    return rel_pos_resized[relative_coords.long()]
+
+
+def add_decomposed_rel_pos(
+    attn: torch.Tensor,
+    q: torch.Tensor,
+    rel_pos_h: torch.Tensor,
+    rel_pos_w: torch.Tensor,
+    q_size: Tuple[int, int],
+    k_size: Tuple[int, int],
+) -> torch.Tensor:
+    """
+    Calculate decomposed Relative Positional Embeddings from :paper:`mvitv2`.
+    https://github.com/facebookresearch/mvit/blob/19786631e330df9f3622e5402b4a419a263a2c80/mvit/models/attention.py   # noqa B950
+    Args:
+        attn (Tensor): attention map.
+        q (Tensor): query q in the attention layer with shape (B, q_h * q_w, C).
+        rel_pos_h (Tensor): relative position embeddings (Lh, C) for height axis.
+        rel_pos_w (Tensor): relative position embeddings (Lw, C) for width axis.
+        q_size (Tuple): spatial sequence size of query q with (q_h, q_w).
+        k_size (Tuple): spatial sequence size of key k with (k_h, k_w).
+
+    Returns:
+        attn (Tensor): attention map with added relative positional embeddings.
+    """
+    print(f"q shape: {q.shape}")
+    q_h, q_w = q_size
+    k_h, k_w = k_size
+    Rh = get_rel_pos(q_h, k_h, rel_pos_h)
+    Rw = get_rel_pos(q_w, k_w, rel_pos_w)
+
+    B, _, dim = q.shape
+    r_q = q.reshape(B, q_h, q_w, dim)
+    rel_h = torch.einsum("bhwc,hkc->bhwk", r_q, Rh)
+    rel_w = torch.einsum("bhwc,wkc->bhwk", r_q, Rw)
+
+    attn = (attn.view(B, q_h, q_w, k_h, k_w) + rel_h[:, :, :, :, None] + rel_w[:, :, :, None, :]).view(
+        B, q_h * q_w, k_h * k_w
+    )
+
+    return attn
